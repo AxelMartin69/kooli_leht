05e9569a7869d2d99712ad14c09fd2461f8ea2f9
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-03-08 10:20:08 +0200

    updated api

--- script/api.js
+++ script/api.js
@@ -1,7 +1,7 @@
 //const tunniplaan = (id = 1529) => {
     var request = new XMLHttpRequest()
 
-    request.open('GET', `https://siseveeb.voco.ee/veebilehe_andmed/tunniplaan?grupp=${id}&nadal=2022-03-07`, true)
+    request.open('GET', `https://siseveeb.voco.ee/veebilehe_andmed/tunniplaan?grupp=1529&nadal=2022-03-07`, true)
     request.onload = function () {
       var data = JSON.parse(this.response)
 

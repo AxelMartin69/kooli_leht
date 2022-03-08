86c499aa8a854e644665d1f61eae286415e371f1
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-03-08 11:36:31 +0200

    removed files

--- script/app.js
+++ script/app.js
@@ -1,1 +1,53 @@
-const tunniplaan = new tunniplaan()
+const app = document.getElementById('home')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+const tunniplaan = (id = 1529, time = '2022-03-07') => {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', `https://siseveeb.voco.ee/veebilehe_andmed/tunniplaan?grupp=${id}&nadal=${time}`, true)
+    request.onload = function () {
+      	var data = JSON.parse(this.response)
+      	if (request.status >= 200 && request.status < 400) {
+      	  	console.log(data)  
+      	  	console.log(data.nadal)
+			const parent = this.el.parentElement;
+            Array.prototype.forEach.call(parent.children, child => {
+              	const card = document.createElement('div')
+              	card.setAttribute('class', 'card')
+				
+              	const h1 = document.createElement('h1')
+              	h1.textContent = tunnid
+				/**
+              	const p = document.createElement('p')
+              	movie.description = movie.description.substring(0, 300)
+              	p.textContent = `${movie.description}...`
+				*/
+              	container.appendChild(card)
+              	card.appendChild(h1)
+              	//card.appendChild(p)
+            })
+      	} else {
+      	  	console.log('error')
+      	}
+    }
+
+    request.send()
+}
+
+function initMap() {
+    // Kooli asukoht
+    const kool = { lat: 58.34985679989212, lng: 26.715514809664125 };
+    // maakaart kooli koha peal
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 16,
+      center: kool,
+    });
+    // Marker kooli kohal
+    const marker = new google.maps.Marker({
+      position: kool,
+      map: map,
+    });
+}
+
